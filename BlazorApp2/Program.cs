272b04c79674a7ab5f9e05adacb4b7b41053fdc2
Diego Saavedra <dsaavedra88@gmail.com>
2023-07-04 11:57:44 -0500

    Implementación de Toast

--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -1,11 +1,15 @@
 using BlazorApp2;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
