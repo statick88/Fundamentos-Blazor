bb75446391b2b8db0046ec72d4b291cf215d6a10
EmilioNacato <ejnacato@espe.edu.ec>
2023-07-05 18:30:51 -0500

    Miproyecto003 finalizado

--- Colaboradores/Emilio-Niacato/MiProyecto003/Program.cs
+++ Colaboradores/Emilio-Niacato/MiProyecto003/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MiProyecto003;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddBlazoredToast();
 
 await builder.Build().RunAsync();
