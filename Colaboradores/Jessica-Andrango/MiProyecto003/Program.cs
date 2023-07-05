b1ebb348064629daf2e152913cf41538c410a868
JessicaAndrango <93225476+JessicaAndrango@users.noreply.github.com>
2023-07-05 18:40:33 -0500

    Cambios Toaster

--- Colaboradores/Jessica-Andrango/MiProyecto003/Program.cs
+++ Colaboradores/Jessica-Andrango/MiProyecto003/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MiProyecto003;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
